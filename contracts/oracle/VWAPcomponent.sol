5ff3f444b64637065db7f09f75e5e7f337f2e449
leo-esaki <leo@arturo.finance>
2022-06-13 10:49:28 -0500

    Implement step2

--- contracts/oracle/VWAPcomponent.sol
+++ contracts/oracle/VWAPcomponent.sol
@@ -4,21 +4,41 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract VWAPcomponent is Ownable {
-    address[] public tokenAddreses;
+    mapping(address => address) tokenPriceWatchList; // iterable map
+    uint256 public listSize;
+    address constant BASE = address(1);
 
-    constructor() {}
+    constructor() {
+        tokenPriceWatchList[BASE] = BASE;
+    }
 
-    function addToken(address _tokenAddr) external onlyOwner {
-        tokenAddreses.push(_tokenAddr);
+    function addToken(address _tokenAddr) external {
+        require(!isToken(_tokenAddr), "VWAPcomponent: token already exists");
+        tokenPriceWatchList[_tokenAddr] = tokenPriceWatchList[BASE];
+        tokenPriceWatchList[BASE] = _tokenAddr;
+        listSize ++;
     }
 
     function removeToken(address _tokenAddr) external onlyOwner {
-        for (uint256 i; i < tokenAddreses.length; i++) {
-            if (tokenAddreses[i] == _tokenAddr) {
-                tokenAddreses[i] = tokenAddreses[tokenAddreses.length - 1];
-                tokenAddreses.pop();
-                break;
+        require(isToken(_tokenAddr), "VWAPcomponent: token doesn't exists");
+        address prevToken = _getPrevToken(_tokenAddr);
+        tokenPriceWatchList[prevToken] = tokenPriceWatchList[_tokenAddr];
+        tokenPriceWatchList[_tokenAddr] = address(0);
+        listSize --;
+    }
+
+    function _getPrevToken(address _tokenAddr) internal view returns (address) {
+        address currentAddr = BASE;
+        while(tokenPriceWatchList[currentAddr] != BASE) {
+            if (tokenPriceWatchList[currentAddr] == _tokenAddr) {
+                return currentAddr;
             }
+            currentAddr = tokenPriceWatchList[currentAddr];
         }
+        return address(0);
+    }
+
+    function isToken(address _tokenAddr) public view returns (bool) {
+        return tokenPriceWatchList[_tokenAddr] != address(0);
     }
 }
