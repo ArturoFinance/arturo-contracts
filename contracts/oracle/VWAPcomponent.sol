9a52c96cb9dd6207b48527d34228b70dd0b0e3d0
leo-esaki <leo@arturo.finance>
2022-06-14 10:04:12 -0500

    Implement TWAP oracle price feed

--- contracts/oracle/VWAPcomponent.sol
+++ contracts/oracle/VWAPcomponent.sol
@@ -2,9 +2,13 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+// import "@chainlink/contracts/src/v0.8/KeeperCompatible.sol";
 
 contract VWAPcomponent is Ownable {
     mapping(address => address) tokenPriceWatchList; // iterable map
+    mapping(address => uint256) tokenPrices;
+    mapping(address => address) proxyAddresses;
     uint256 public listSize;
     address constant BASE = address(1);
 
@@ -41,4 +45,52 @@ contract VWAPcomponent is Ownable {
     function isToken(address _tokenAddr) public view returns (bool) {
         return tokenPriceWatchList[_tokenAddr] != address(0);
     }
+
+    function _getLatestTokenPrice(AggregatorV3Interface tokenPriceFeed)
+        internal
+        view
+        returns (uint256)
+    {
+        (
+            ,
+            /*uint80 roundID*/
+            int price, /*uint startedAt*/
+            ,
+            ,
+
+        ) = /*uint timeStamp*/
+            /*uint80 answeredInRound*/
+            tokenPriceFeed.latestRoundData();
+        return uint256(price);
+    }
+
+    function getStoredTokenPrices() external view returns (uint256[] memory) {
+        uint256[] memory priceLists = new uint256[](listSize);
+        address currentAddress = tokenPriceWatchList[BASE];
+
+        for(uint256 i = 0; currentAddress != BASE; ++i) {
+            uint256 price = getTokenPrice(currentAddress);
+            priceLists[i] = price;
+            currentAddress = tokenPriceWatchList[currentAddress];
+        }
+
+        return priceLists;
+    }
+
+    function getTokenPrice(address token) public view returns (uint256) {
+        require(proxyAddresses[token] != address(0), "This token doesn't have its proxy address. Add proxy address.");
+
+        address proxy = proxyAddresses[token];
+        AggregatorV3Interface tokenPriceFeed = AggregatorV3Interface(proxy);
+        uint256 price = _getLatestTokenPrice(tokenPriceFeed);
+        return price;
+    }
+
+    function addTokenProxy(address _tokenAddr, address _proxyAddr) external {
+        require(_tokenAddr != address(0), "Invalid token address");
+        require(_proxyAddr != address(0), "Invalid proxy address");
+
+        proxyAddresses[_tokenAddr] = _proxyAddr;
+    }
+    
 }
