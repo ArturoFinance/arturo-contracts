a52e42a496388eef5c9dab1a4ddabd6c57ab8d0b
leo-esaki <leo@arturo.finance>
2022-07-08 05:56:23 -0500

    Group liquidity functionalites together

--- contracts/LiquidityProvide.sol
+++ contracts/LiquidityProvide.sol
@@ -80,7 +80,7 @@ contract LiquidityProvide {
         }
     }
 
-    function addLiquidityToUniswap(
+    function addLiquidity(
         address _owner,
         address _tokenA,
         address _tokenB,
@@ -97,7 +97,22 @@ contract LiquidityProvide {
         IERC20(_tokenA).transferFrom(_owner, address(this), _amountA);
         IERC20(_tokenB).transferFrom(_owner, address(this), _amountB);
 
-        (uint amountA, uint amountB, uint liquidity) = IUniswapV2Router02(UNISWAP_V2_ROUTER)
+        if (pType == ProtocolTypes.UniswapV2) {
+            (uint amountA, uint amountB, uint liquidity) = IUniswapV2Router02(UNISWAP_V2_ROUTER)
+                .addLiquidity(
+                    _tokenA,
+                    _tokenB,
+                    _amountA,
+                    _amountB,
+                    1,
+                    1,
+                    address(this),
+                    block.timestamp
+                );
+
+            emit LiquidityAdded(UNISWAP_V2_ROUTER, amountA, amountB, liquidity);
+        } else if (pType == ProtocolTypes.Apeswap) {
+            (uint amountA, uint amountB, uint liquidity) = IApeRouter(APESWAP_ROUTER)
             .addLiquidity(
                 _tokenA,
                 _tokenB,
@@ -109,7 +124,8 @@ contract LiquidityProvide {
 	            block.timestamp
             );
 
-        emit LiquidityAdded(UNISWAP_V2_ROUTER, amountA, amountB, liquidity);
+            emit LiquidityAdded(APESWAP_ROUTER, amountA, amountB, liquidity);
+        }
     }
 
     function removeLiquidityFromUniswap(
@@ -119,7 +135,6 @@ contract LiquidityProvide {
         address _pair,
         ProtocolTypes pType
     ) external {
-        require(pType == ProtocolTypes.UniswapV2, "Unknown protocol type");
         require(_tokenA != address(0), "Workflow: invalid token address");
         require(_tokenB != address(0), "Workflow: invalid token address");
         require(_pair != address(0), "Workflow: invalid pair address");
@@ -127,76 +142,30 @@ contract LiquidityProvide {
         uint liquidity = IERC20(_pair).balanceOf(address(this));
         require(liquidity != 0, "Workflow: has no balance");
 
-        (uint amountA, uint amountB) = IUniswapV2Router02(UNISWAP_V2_ROUTER).removeLiquidity(
-            _tokenA,
-            _tokenB,
-            liquidity,
-            1,
-            1,
-            _owner,
-            block.timestamp
-        );
-
-        emit LiquidityRemoved(UNISWAP_V2_ROUTER, amountA, amountB);
-    }
-
-    function addLiquidityToApeswap(
-        address _owner,
-        address _tokenA,
-        address _tokenB,
-        uint _amountA,
-        uint _amountB,
-        ProtocolTypes pType
-    ) external {
-        require(pType == ProtocolTypes.Apeswap, "Unknown protocol type");
-        require(_tokenA != address(0), "Workflow: invalid token address");
-        require(_tokenB != address(0), "Workflow: invalid token address");
-        require(_amountA != 0, "Workflow: token amount should not be zero");
-        require(_amountB != 0, "Workflow: token amount should not be zero");
-
-        IERC20(_tokenA).transferFrom(_owner, address(this), _amountA);
-        IERC20(_tokenB).transferFrom(_owner, address(this), _amountB);
-
-        (uint amountA, uint amountB, uint liquidity) = IApeRouter(APESWAP_ROUTER)
-            .addLiquidity(
+        if (pType == ProtocolTypes.UniswapV2) {
+            (uint amountA, uint amountB) = IUniswapV2Router02(UNISWAP_V2_ROUTER).removeLiquidity(
                 _tokenA,
                 _tokenB,
-                _amountA,
-                _amountB,
+                liquidity,
                 1,
                 1,
-                address(this),
-	            block.timestamp
+                _owner,
+                block.timestamp
             );
 
-        emit LiquidityAdded(APESWAP_ROUTER, amountA, amountB, liquidity);
+            emit LiquidityRemoved(UNISWAP_V2_ROUTER, amountA, amountB);
+        } else if (pType == ProtocolTypes.Apeswap) {
+            (uint amountA, uint amountB) = IApeRouter(APESWAP_ROUTER).removeLiquidity(
+                _tokenA,
+                _tokenB,
+                liquidity,
+                1,
+                1,
+                _owner,
+                block.timestamp
+            );
+
+            emit LiquidityRemoved(APESWAP_ROUTER, amountA, amountB);
+        }
     }
-
-    function removeLiquidityFromApeswap(
-        address _owner,
-        address _tokenA,
-        address _tokenB,
-        address _pair,
-        ProtocolTypes pType
-    ) external {
-        require(pType == ProtocolTypes.Apeswap, "Unknown protocol type");
-        require(_tokenA != address(0), "Workflow: invalid token address");
-        require(_tokenB != address(0), "Workflow: invalid token address");
-        require(_pair != address(0), "Workflow: invalid pair address");
-
-        uint liquidity = IERC20(_pair).balanceOf(address(this));
-        require(liquidity != 0, "Workflow: has no balance");
-
-        (uint amountA, uint amountB) = IApeRouter(APESWAP_ROUTER).removeLiquidity(
-            _tokenA,
-            _tokenB,
-            liquidity,
-            1,
-            1,
-            _owner,
-            block.timestamp
-        );
-
-        emit LiquidityRemoved(APESWAP_ROUTER, amountA, amountB);
-    }
-}   
+}
