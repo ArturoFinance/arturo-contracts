145bb68abe9f6775418b47c59edf92bfcfe16eb3
leo-esaki <leo@arturo.finance>
2022-07-18 09:02:34 -0500

    Implement swap function on Firebird

--- contracts/SwapEngine.sol
+++ contracts/SwapEngine.sol
@@ -65,12 +65,25 @@ interface IOneinchRouter {
     ) external returns (uint256 returnAmount);
 }
 
+interface IFireBirdRouter {
+    function swapExactTokensForTokens(
+        address tokenIn,
+        address tokenOut,
+        uint amountIn,
+        uint amountOutMin,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external returns (uint[] memory amounts);
+}
+
 contract SwapEngine {
     address private constant APESWAP_ROUTER = 0xC0788A3aD43d79aa53B09c2EaCc313A787d1d607;
     address private constant UNISWAP_V2_ROUTER = 0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45;
     address private constant UNISWAP_V3_ROUTER = 0xE592427A0AEce92De3Edee1F18E0157C05861564;
     address private constant SUSHISWAP_ROUTER = 0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506;
     address private constant ONEINCH_ROUTER = address(0);
+    address private constant FIREBIRD_ROUTER = address(0);
     address private constant WMATIC = 0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889;
 
     enum SwapEngines {
@@ -78,13 +91,15 @@ contract SwapEngine {
         UniswapV2,
         UniswapV3,
         Sushiswap,
-        Oneinch
+        Oneinch,
+        Firebird
     }
 
     event TokensSwappedOnUniswapV2(address tokenIn, address tokenOut, address to);
     event TokensSwappedOnUniswapV3(address tokenIn, address tokenOut, address to);
     event TokensSwappedOnApeswap(address tokenIn, address tokenOut, address to);
     event TokensSwappedOnSushiswap(address tokenIn, address tokenOut, address to);
+    event TokenSwappedOnFirebird(address tokenIn, address tokenOut, address to);
     event UnowappedOnOneinch(address tokenIn, bytes32[] pool, uint256 amountOut);
     event AggregationSwappedOnOneinch(address tokenIn, address tokenOut, uint256 amountOut);
 
@@ -93,6 +108,7 @@ contract SwapEngine {
     event TokensApprovedOnApeswap(address protocol, address token, uint256 amount);
     event TokensApprovedOnSushiswap(address protocol, address token, uint256 amount);
     event TokensApprovedOnOneinch(address protocol, address token, uint256 amount);
+    event TokensApprovedOnFirebird(address protocol, address token, uint256 amount);
 
     constructor() {}
 
@@ -115,6 +131,8 @@ contract SwapEngine {
             emit TokensApprovedOnSushiswap(SUSHISWAP_ROUTER, _tokenIn, _amountIn);
         } else if (engineType == SwapEngines.Oneinch) {
             IERC20(_tokenIn).approve(ONEINCH_ROUTER, _amountIn);
+        } else if (engineType == SwapEngines.Firebird) {
+            IERC20(_tokenIn).approve(FIREBIRD_ROUTER, _amountIn);
         }
     }
 
@@ -141,7 +159,7 @@ contract SwapEngine {
         uint _amountIn,
         SwapEngines engineType
     ) external returns (uint amountOut) {
-        require(engineType == SwapEngines.UniswapV2, "Please call a reasonable function");
+        require(engineType == SwapEngines.UniswapV2, "Unknown swap function");
         IERC20(_tokenIn).transferFrom(_owner, address(this), _amountIn);
 
         // address[] memory path = _getPath(_tokenIn, _tokenOut);
@@ -176,7 +194,7 @@ contract SwapEngine {
         uint256 _amountIn,
         SwapEngines engineType
     ) external returns (uint256 amountOut) {
-        require(engineType == SwapEngines.UniswapV3, "Please call a reasonable function");
+        require(engineType == SwapEngines.UniswapV3, "Unknown swap function");
         // Transfer the specified amount of _tokenIn to this contract.
         TransferHelper.safeTransferFrom(_tokenIn, _owner, address(this), _amountIn);
         // Naively set amountOutMinimum to 0. In production, use an oracle or other data source to choose a safer value for amountOutMinimum.
@@ -228,7 +246,7 @@ contract SwapEngine {
         uint _amountIn,
         SwapEngines engineType
     ) external {
-        require(engineType == SwapEngines.Sushiswap, "Please call a reasonable function");
+        require(engineType == SwapEngines.Sushiswap, "Unknown swap function");
 
         IERC20(_tokenIn).transferFrom(_owner, address(this), _amountIn);
 
@@ -245,6 +263,32 @@ contract SwapEngine {
         emit TokensSwappedOnSushiswap(_tokenIn, _tokenOut, _owner);
     }
 
+    function swapOnFirebird(
+        address _owner,
+        address _tokenIn,
+        address _tokenOut,
+        uint _amountIn,
+        SwapEngines engineType
+    ) external {
+        require(engineType == SwapEngines.Firebird, "Unknow swap functions");
+
+        IERC20(_tokenIn).transferFrom(_owner, address(this), _amountIn);
+
+        address [] memory path = _getPath(_tokenIn, _tokenOut);
+
+        IFireBirdRouter(FIREBIRD_ROUTER).swapExactTokensForTokens(
+            _tokenIn,
+            _tokenOut,
+            _amountIn,
+            1,
+            path,
+            _owner,
+            block.timestamp
+        );
+
+        emit TokenSwappedOnFirebird(_tokenIn, _tokenOut, _owner);
+    }
+
     function unoswapOnOneinch(
         address _owner,
         address _tokenIn,
@@ -252,7 +296,7 @@ contract SwapEngine {
         bytes32[] calldata _pools,
         SwapEngines engineType
     ) external {
-        require(engineType == SwapEngines.Oneinch, "Please call a reasonable function");
+        require(engineType == SwapEngines.Oneinch, "Unknown swap function");
         IERC20(_tokenIn).transferFrom(_owner, address(this), _amountIn);
 
         uint256 amountOut = IOneinchRouter(ONEINCH_ROUTER).unoswap(
@@ -272,7 +316,7 @@ contract SwapEngine {
         uint256 _amountIn,
         SwapEngines engineType
     ) external {
-        require(engineType == SwapEngines.Oneinch, "Please call a reasonable function");
+        require(engineType == SwapEngines.Oneinch, "Unknown swap function");
         IERC20(_tokenIn).transferFrom(_owner, address(this), _amountIn);
 
         (uint256 amountOut, , ) = IOneinchRouter(ONEINCH_ROUTER).swap(
